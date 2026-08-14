8c87c4ad751bba7964c29c34980ac7e208b3c01c
codingwithrahat <codingwithrahat@gmail.com>
2026-08-15 02:16:17 +0600

    hf local model

---
+++ LangChain Models/2. ChatModels/5_chatmodels_hf_local.py
@@ -0,0 +1,20 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+import os
+
+os.environ["HF_HOME"] = "E:/huggingface_cache"
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    pipeline_kwargs={
+        "temperature": 0.5,
+        "max_new_tokens": 100,
+        "do_sample": True,
+    }
+)
+
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What is the capital of Bangladesh?")
+
+print(result.content)
