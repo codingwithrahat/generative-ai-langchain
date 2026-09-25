952f1cc88b64d1ba2657e929dbd1f1b519c76b37
codingwithrahat <codingwithrahat@gmail.com>
2026-09-25 20:34:02 +0600

    download model

--- LangChain Models/3. EmbeddedModels/3_embedding_hf_local.py
+++ LangChain Models/3. EmbeddedModels/3_embedding_hf_local.py
@@ -1,7 +1,7 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 import os
 
-os.environ["HF_HOME"] = "E:/huggingface_cache"
+os.environ["HF_HOME"] = "/home/rahat/local_models/huggingface_cache"
 
 embedding = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2')
 
